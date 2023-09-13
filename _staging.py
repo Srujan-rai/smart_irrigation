087e8c790b5cec21661285fc569c87b98fb67ba7
Srujan-rai <raisrujan@gmail.com>
2023-09-13 13:58:14 +0530

    app .py correction

--- _staging.py
+++ _staging.py
@@ -4,6 +4,7 @@ import pandas as pd
 
 app = Flask(__name__)
 
+
 model = joblib.load('logistic_regression_model.pkl')
 
 @app.route('/', methods=['GET', 'POST'])
@@ -20,6 +21,7 @@ def predict():
             'soil_moisture': [soil_moisture]
         })
 
+
         prediction = model.predict(input_data)
 
         pump_status = 'ON' if prediction[0] == 1 else 'OFF'
@@ -28,10 +30,5 @@ def predict():
 
     return render_template('index.html', prediction=None)
 
-# This block is used for running the app with Gunicorn on Render
 if __name__ == '__main__':
-    # Use Gunicorn as the web server
-    import os
-    host = '0.0.0.0'
-    port = int(os.environ.get('PORT', 8000))  # Use the PORT environment variable provided by Render
-    app.run(debug=False, host=host, port=port)
+    app.run(debug=False,host='0.0.0.0')
